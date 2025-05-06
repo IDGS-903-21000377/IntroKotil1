250ae4e63f918348cb73868180a1d6edd35a75c5
LuisGerardoLopez <21000377@alumnos.utleon.edu.mx>
2025-05-05 22:26:12 -0600

    Initial commit

---
+++ app/src/main/java/com/example/introkotil/_temaskotil/Tema2-if.kt
@@ -0,0 +1,20 @@
+package com.example.introkotil._temaskotil
+
+fun main(){
+    print("ingresa el sueldo del empleado:")
+
+    val sueldo = readln().toDouble()
+
+
+ if (sueldo > 3000){
+     println("Debe pagar impuestos")
+ }
+    else{
+        println("no debe pagar impuestos ")
+
+    }
+
+
+
+
+}
